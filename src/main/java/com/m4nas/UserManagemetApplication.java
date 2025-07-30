e74883f34f7a539e66c9dd16eb362022bf617823
md4nas <md.anas1028@gmail.com>
2025-07-31 00:25:16 +0530

    enhanced the Login,Registration UI and added animations

--- src/main/java/com/m4nas/UserManagemetApplication.java
+++ src/main/java/com/m4nas/UserManagemetApplication.java
@@ -1,13 +1,23 @@
 package com.m4nas;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class UserManagemetApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserManagemetApplication.class, args);
+	}
+
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
 
 }

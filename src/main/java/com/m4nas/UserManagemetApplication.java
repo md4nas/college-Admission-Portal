842804d8e746375ba1d06d8c5588253d97628151
md4nas <md.anas1028@gmail.com>
2025-07-31 17:11:55 +0530

    feat: configure application with OAuth2 and REST template support
    - Add RestTemplate bean for OAuth2 HTTP operations
    - Remove duplicate password encoder bean definition
    - Configure main application class for Spring Boot startup
    - Support OAuth2 integration requirements

--- src/main/java/com/m4nas/UserManagemetApplication.java
+++ src/main/java/com/m4nas/UserManagemetApplication.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
@@ -19,5 +20,6 @@ public class UserManagemetApplication {
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
 	}
+
 
 }
